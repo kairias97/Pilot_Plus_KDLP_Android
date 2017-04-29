0ee81004ab9ebd74e87cf7c88f0cc67e524f3ff8
Kevin Irias <kevin.irias@inaxir.com>
2017-04-29 11:23:30 -0600

    added travels list activity and design

---
+++ app/src/main/java/com/kairias97/pilotpluskdlp/activities/TravelsListActivity.java
@@ -0,0 +1,20 @@
+package com.kairias97.pilotpluskdlp.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.kairias97.pilotpluskdlp.R;
+
+public class TravelsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travels_summary);
+    }
+
+}
