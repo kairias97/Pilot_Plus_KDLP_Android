4ddc676d6ad0683926b5423c4c28a88b02833c02
Kevin Irias <kevin.irias@inaxir.com>
2017-04-29 19:34:05 -0600

    Fixed google maps implementation

--- app/src/main/java/com/kairias97/pilotpluskdlp/activities/TravelsListActivity.java
+++ app/src/main/java/com/kairias97/pilotpluskdlp/activities/TravelsListActivity.java
@@ -1,5 +1,6 @@
 package com.kairias97.pilotpluskdlp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.kairias97.pilotpluskdlp.FormAddActivity;
 import com.kairias97.pilotpluskdlp.R;
 
 public class TravelsListActivity extends AppCompatActivity {
@@ -15,6 +17,14 @@ public class TravelsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_travels_summary);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_add_travel);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TravelsListActivity.this, FormAddActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
